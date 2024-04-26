739ff23ac0e191f33c8a3c8d0b54a1c39f3cc488
Tofu Shila <shilatof@pr-a6.kzn.21-school.ru>
2024-04-26 18:07:04 +0300

    ex07

--- ex07/main.go
+++ ex07/main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"runtime"
 	"sync"
-	"time"
 )
 
-// Завершение работы горутины с помощью sync.WaitGroup
+// Реализовать конкурентную запись данных в map.
 func main() {
-	
 
+	myMap := make(map[int]int)
+
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	//ch := make(chan int)
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+
+		wg.Add(1)
+		go worker(myMap, &wg, &mu)
+
+	}
+	wg.Wait()
+	fmt.Println(myMap)
 }
 
+func worker(myMap map[int]int, wg *sync.WaitGroup, mu *sync.Mutex) {
+	defer wg.Done()
 
+	for i := 10; i < 20; i++ {
+		mu.Lock()
+		myMap[i] = i * i
+		mu.Unlock()
+	}
 
-//
+}
